f3dd7a9a71058543f9cbda7e595e14353f3602e4
Robert Perona <robert.perona@gmail.com>
2020-05-07 08:23:54 -0700

    Switch from exe to msbuilt task using netstandard works with build in vs but still has issue withdotnet build

---
+++ src/Pen/PenGenerate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+
+namespace Pen
+{
+    public class PenGenerate : Task
+    {
+        public override bool Execute()
+        {
+            Generate.Execute(Namespace, InputDll, OutputDirectory);
+            return true;
+        }
+
+        [Required]
+        public string Namespace { get; set; }
+
+        [Required]
+        public string InputDll { get; set; }
+
+        [Required]
+        public string OutputDirectory { get; set; }
+    }
+}
